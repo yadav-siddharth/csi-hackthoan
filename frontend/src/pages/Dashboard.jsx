7a46c84e2b185ba00b7e7e789bc9a3ff2506a23a
siddharth <siddharth@gmail.com>
2023-01-18 11:17:12 +0530

    my fist commmit

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div>
+        <div className="hero">
+          <div className="hero-inside">
+
+          </div>
+          <div className="hero-image">
+            
+          </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Dashboard
